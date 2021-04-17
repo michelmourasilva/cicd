3b37de07cbb1248b4b6594d95713eea978cd06cf
Michel Moura Silva <37510050+michelmourasilva@users.noreply.github.com>
2021-04-17 04:46:32 -0300

    Create entrypoint.sh

---
+++ JenkinsAgentImage/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+set -e
+#sudo dockerd --insecure-registry=172.19.0.7:5000
+echo "starting dockerd..."
+sudo dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --insecure-registry=172.19.0.7:5000 --storage-driver=vfs &
+echo "starting jnlp slave..."
+exec java -jar /usr/share/jenkins/slave.jar \
+	-jnlpUrl $JENKINS_URL/computer/$JENKINS_SLAVE_NAME/slave-agent.jnlp \
+	-secret $JENKINS_SLAVE_SECRET
+	
+	
