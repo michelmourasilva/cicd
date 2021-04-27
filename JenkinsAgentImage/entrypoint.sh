9059a8463828b5884ca4389e00c11bd48ba98337
Michel Moura Silva <37510050+michelmourasilva@users.noreply.github.com>
2021-04-26 22:27:54 -0300

    Update entrypoint.sh

--- JenkinsAgentImage/entrypoint.sh
+++ JenkinsAgentImage/entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 #sudo dockerd --insecure-registry=172.19.0.7:5000
 echo "starting dockerd..."
-sudo dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --insecure-registry=172.19.0.7:5000 --storage-driver=vfs &
+sudo dockerd --host=unix:///var/run/docker.sock --host=tcp://0.0.0.0:2375 --insecure-registry=172.19.0.3:5000 --storage-driver=vfs &
 echo "starting jnlp slave..."
 exec java -jar /usr/share/jenkins/slave.jar \
 	-jnlpUrl $JENKINS_URL/computer/$JENKINS_SLAVE_NAME/slave-agent.jnlp \
